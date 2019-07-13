9e6af9a898b98b460e77b5aebef11d16d93029fb
kembo <lupin@kembo.org>
2019-07-13 14:38:06 +0900

    基本的な Grass の読込のテストを作成

--- test/grass_test.rb
+++ test/grass_test.rb
@@ -1,4 +1,19 @@
 require 'test/unit'
 require 'grass'
 
-GrassCode
+class TestGrassCode < Test::Unit::TestCase
+  data(
+    "simplest code" => ["w", "[Abs(1, [])]"],
+    "simplest code2"=> ["wv", "[Abs(1, [])]"],
+    "long function" => ["wwwWWWwwWWWWwwWw", "[Abs(3, [App(3, 2), App(4, 2), App(1, 1)])]"],
+    "multi function"=> ["wWwvwWw", "[Abs(1, [App(1, 1)]), Abs(1, [App(1, 1)])]"],
+    "funcs & apps"  => ["wwWwwvWwwwvWWwwwWWWwvwWw",
+                        "[Abs(2, [App(1, 2)]), App(1, 3), App(2, 3), App(3, 1), Abs(1, [App(1, 1)])]"]
+  )
+  def test_new_simple(data)
+    code_string, structual_string = data
+    code = GrassCode.new(code_string)
+    assert_equal(code_string, code.to_s)
+    assert_equal(structual_stringm, code.parse_tree.to_s)
+  end
+end
